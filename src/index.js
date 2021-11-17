99187c95df91840e8991c1a0191d0a0f79005bfa
Dmitriy Korolov <adrorecords@gmail.com>
2021-11-17 12:39:44 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './sass/main.scss';
 
 import { Notify } from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { getRefs } from './js/getRefs';
 import { getImg } from './js/getImg';
 import { isEndOfImg } from './js/isEndOfImg';
+import { markup } from './js/markup';
 
 const refs = getRefs();
 let page = 1;
@@ -27,7 +26,16 @@ function onSubmitClick(event) {
 
     if (searchQuery === '') { return Notify.failure('Please enter your search data.') };
     event.target.reset();
-    getImg(searchQuery, page).then(markup);
+    getImg(searchQuery, page).then(rev => {
+        const imgArray = rev.data.hits;
+        if (imgArray.length === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
+        totalHits = rev.data.totalHits;
+        Notify.success(`Hooray! We found ${totalHits} images.`);
+        markup(rev);
+    });
     refs.loadMoreBtn.classList.remove('is-hidden');
     isEndOfImg(page, totalHits);
     page += 1;    
@@ -38,40 +46,3 @@ function onMoreLoadBtnClick() {
     isEndOfImg(page, totalHits);
     page += 1;    
 }
-
-function markup({ data }) {
-    const imgArray = data.hits;
-    totalHits = data.totalHits;
-    if (page === 2) { Notify.success(`Hooray! We found ${totalHits} images.`)};
-    if (imgArray.length === 0) { return Notify.failure('Sorry, there are no images matching your search query. Please try again.') }
-      
-    const galleryMarkup = imgArray.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>
-  `<div class="photo-card">
-  <a class="gallery__item" href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="354" height="225"/></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes </br><span class='text'>${likes}</span></b>
-    </p>
-    <p class="info-item">
-      <b>Views  </br><span class='text'>${views}</span></b>
-    </p>
-    <p class="info-item">
-      <b>Comments  </br><span class='text'>${comments}</span></b>
-    </p>
-    <p class="info-item">
-      <b>Downloads  </br><span class='text'>${downloads}</span></b>
-    </p>
-  </div>
-</div>`
-    ).join('');
-        
-refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
-let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    captionPosition: 'botton',
-});
-}
-
---
+++ src/js/markup.js
@@ -0,0 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getRefs } from "./getRefs";
+
+const refs = getRefs();
+
+export function markup(images) {
+    const imgArray = images.data.hits;
+            
+    const galleryMarkup = imgArray.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>
+  `<div class="photo-card">
+  <a class="gallery__item" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="354" height="225"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes </br><span class='text'>${likes}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Views  </br><span class='text'>${views}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Comments  </br><span class='text'>${comments}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Downloads  </br><span class='text'>${downloads}</span></b>
+    </p>
+  </div>
+</div>`
+    ).join('');
+        
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'botton',
+});
+}
