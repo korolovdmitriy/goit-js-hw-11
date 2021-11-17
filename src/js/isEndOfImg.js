46af949f3f445e2e29225b8c43048fc17c4c575b
Dmitriy Korolov <adrorecords@gmail.com>
2021-11-17 15:02:40 +0200

    update

--- src/js/isEndOfImg.js
+++ src/js/isEndOfImg.js
@@ -3,7 +3,7 @@ import { Notify } from "notiflix";
 
 const refs = getRefs();
 export function isEndOfImg(page, totalHits) {
-    if (page*40 > totalHits) {
+    if (page*40 >= totalHits) {
         refs.loadMoreBtn.classList.add('is-hidden');   
         Notify.success("We're sorry, but you've reached the end of search results.");         
     }
